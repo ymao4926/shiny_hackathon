62660b3bb95020b38bab175938c5574d129da414
wharvey31 <williamthomasharvey@gmail.com>
2022-09-21 09:44:33 -0700

    Added zoom

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(GenomicRanges)
 library(gggenes)
 library(tidyverse)
 library(readr)
+# library(Cairo)
 
 
 # Source helper functions -----
@@ -29,7 +30,7 @@ ui <- fluidPage(
 						## Input BED file
 						fileInput(
 							"bed",
-						  	"BED file input",
+						  "BED file input",
 							multiple=FALSE, 
 							buttonLabel="Browse",
 							placeholder="No file selected",
@@ -52,62 +53,80 @@ ui <- fluidPage(
 							),
 						# Not so sure about this part yet
 						selectInput(
-						  "select_graph","Graph",selected = NULL, multiple = FALSE,list("outputfile1","outputfile2")),
-						
-						
+						  "select_graph",
+						  "Graph",
+						  selected = NULL, 
+						  multiple = FALSE,
+						  list("outputfile1","outputfile2")
+						  ),
 						## Download Fasta file and Bed file
-						downloadButton("Fasta_download", "FASTA file Download",icon = shiny::icon("download")),
-						downloadButton("BED_download", "BED file Download",icon = shiny::icon("download"))
+						downloadButton("Fasta_download", 
+						               "FASTA file Download",
+						               icon = shiny::icon("download")
+						               ),
+						downloadButton("BED_download", 
+						               "BED file Download",
+						               icon = shiny::icon("download"))
 						),
 				
 				mainPanel(
-					        "Graph Visualization Window",
+					        # "Graph Visualization Window",
 
 					        ## Plot Ouput of graphic visualization
 					        plotOutput(
-							"ggdag",
-							"Graph Visualization Window",
-							width="100%",
-							height="400px",
-							brush=brushOpts(id="plot_brush")
+      							"ggdag",
+      							"Graph Visualization Window",
+      							width="100%",
+      							height="400px",
+      							brush=brushOpts(id="graph_brush", resetOnNew = TRUE),
+							      click="graph_click",
+      							dblclick = "graph_dblclick",
 						        ),
-
+					        tableOutput("graph_point"),
 					        ## Plot Ouput of linear visualization
-					        "Linear Visualization Window",
+					        # "Linear Visualization Window",
 					        plotOutput(
-							"bed_plots",width="100%",height="100px",click = "plot_click",
-							dblclick = "plot_dblclick",
-							hover = "plot_hover",
-							brush = "plot_brush"
+						        	"bed_plots",
+						        	width="100%",
+						        	height="100px",
+						        	click = "linear_click",
+						        	dblclick = "linear_dblclick",
+				        			hover = "linear_hover",
+						        	brush = "linear_brush"
 						        ),
-
 					        ## Ouput of user graphical interactive information
-					        verbatimTextOutput("info"),
-					        tableOutput("contents"),
+					        # verbatimTextOutput("info"),
 					)
 		)
 
 # Server logic ----
 server <- function(input, output) {
-  spacer.width <- reactive({
-    input$spacer
-    })
 	# Read the rGFA file
-	output$contents <- renderTable({
-		req(input$rgfaFile)
-	  df <- readGfa(gfa.file = input$rgfaFile$datapath, 
-	                store.fasta = 'FALSE')
-		return(df$segments)
+	output$graph_point <- renderTable({
+		req(input$graph_click)
+	  nearPoints(df$segments, input$plot_click, xvar = "x", yvar = "rank")
 	})
-
+	
+	ranges <- reactiveValues(x = NULL, y = NULL)
+	
+	observeEvent(input$graph_dblclick, {
+	  brush <- input$graph_brush
+	  if (!is.null(brush)) {
+	    ranges$x <- c(brush$xmin, brush$xmax)
+	  } else {
+	    ranges$x <- NULL
+	  }
+	})
 	# Plot the graph
 	output$ggdag <- renderPlot({
+	  # Wait for rgfa input
 	  req(input$rgfaFile)
+	  # Process GFA
 	  df <- readGfa(gfa.file = input$rgfaFile$datapath, 
 	                store.fasta = 'FALSE')
-	  plotGfa(gfa.tbl=df)
+	  # Plot df and add cartesian 
+	  plotGfa(gfa.tbl=df) + coord_cartesian(xlim = ranges$x, ylim = NULL, expand = FALSE)
 	  	  })
-					  
 	  ## Linear visualization
 	  output$bed_plots <- renderPlot({
 		  req(input$bed)
@@ -124,7 +143,6 @@ server <- function(input, output) {
 		  theme(legend.position="none")
 	  	  
 	  })
-					 
 	  output$info <- renderText({
 		  xy_str <- function(e) {
 			  if(is.null(e)) return("NULL\n")
@@ -137,10 +155,10 @@ server <- function(input, output) {
 		  }
 		  
 		  paste0(
-			  "click: ", xy_str(input$plot_click),
-			  "dblclick: ", xy_str(input$plot_dblclick),
-			  "hover: ", xy_str(input$plot_hover),
-			  "brush: ", xy_range_str(input$plot_brush)
+			  "click: ", xy_str(input$linear_click),
+			  "dblclick: ", xy_str(input$linear_dblclick),
+			  "hover: ", xy_str(input$linear_hover),
+			  "brush: ", xy_range_str(input$linear_brush)
 		  )
 	  })
 }
